04435d845082ea42cb8e6e3a78487d2c529b7e2d
yokoyama shunya <nogamoe2400hs@gmail.com>
2024-10-19 16:01:48 +0900

    feat: チェックしたカテゴリのキャラクターを表示

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-const ALL_API = "https://ihatov08.github.io/kimetsu_api/api/all.json";
-      HASHIRA_API = "https://ihatov08.github.io/kimetsu_api/api/hashira.json";
-      ONI_API = "https://ihatov08.github.io/kimetsu_api/api/oni.json";
-      KISATSUTAI_API = "https://ihatov08.github.io/kimetsu_api/api/kisatsutai.json";
-const $INPUTS = document.querySelectorAll("input[name=characters]");
+const APIs = [
+  {
+    category: "all", url:  "https://ihatov08.github.io/kimetsu_api/api/all.json"
+  },
+  {
+    category: "hashira", url: "https://ihatov08.github.io/kimetsu_api/api/hashira.json"
+  },
+  {
+    category: "oni", url: "https://ihatov08.github.io/kimetsu_api/api/oni.json"
+  },
+  {
+    category: "kisatsutai", url: "https://ihatov08.github.io/kimetsu_api/api/kisatsutai.json"
+  }
+]
 const $UL_ALL = document.getElementById("all");
 const $UL_HASHIRA = document.getElementById("hashira");
 const $UL_ONI = document.getElementById("oni");
@@ -12,15 +21,16 @@ const $LI_HASHIRA = document.createElement("li");
 const $LI_ONI = document.createElement("li");
 const $LI_KISATSUTAI = document.createElement("li");
 const $DIV_CHARACTERS_CONTAINER = document.getElementById("character-container");
+const $INPUTS = document.getElementsByName("character");
 
-let $DIV_IMG, $DIV_CHARACTERS_COLUMN;
+let $DIV_IMG, $DIV_CHARACTERS_COLUMN, $H2, $IMG, $SPAN;
 
 const loading = () => {
   return new Promise((resolve) => {
-    console.log('Loading');
+    $P.innerText = "Loading...";
     setTimeout(() => {
       resolve();
-    }, 0)
+    }, 3000)
   })
 }
 
@@ -30,64 +40,61 @@ const fetchCharacters = async (api) => {
   return characters;
 }
 
+const removeCurrentElements = async () => {
+  while( $DIV_CHARACTERS_CONTAINER.firstChild ){
+    $DIV_CHARACTERS_CONTAINER.removeChild( $DIV_CHARACTERS_CONTAINER.firstChild );
+  }
+}
 
 const createElements = async (characters) => {
+  $P.innerText = "";
   const groups = Object.groupBy(characters, ({category}) => category);
   const categories = Object.keys(groups);
   categories.forEach(category => {
     $DIV_CHARACTERS_COLUMN = document.createElement("div");
-    const $H2 = document.createElement("h2");
+    $H2 = document.createElement("h2");
     $DIV_IMG = document.createElement("div");
     $DIV_CHARACTERS_COLUMN.appendChild($H2);
     $DIV_CHARACTERS_COLUMN.appendChild($DIV_IMG);
+    $DIV_IMG.id = "character-img";
     $DIV_CHARACTERS_COLUMN.className = "character-column";
     $H2.innerText = category;
     $DIV_CHARACTERS_CONTAINER.appendChild($DIV_CHARACTERS_COLUMN);
     characters.forEach(character => {
-      const $IMG = document.createElement("img");
+      $SPAN = document.createElement("span");
+      $IMG = document.createElement("img");
+      $SPAN.innerText = character.name;
       $IMG.style.width = "100%";
       $IMG.style.height = "auto";
       $IMG.src = "https://ihatov08.github.io" + character.image;
-      if (category == character.category) $DIV_IMG.appendChild($IMG);
+      if (category == character.category) {
+        $DIV_IMG.appendChild($SPAN);
+        $DIV_IMG.appendChild($IMG)
+      };
     })
   })
 
   $DIV_CHARACTERS_CONTAINER.style.display = 'flex';
   $DIV_CHARACTERS_CONTAINER.style.justifyContent = 'space-between';
   $DIV_CHARACTERS_CONTAINER.style.alignItems = 'flex-start';
-  $DIV_CHARACTERS_COLUMN.appendChild($DIV_CATEGORY);
-  $DIV_CATEGORY.appendChild($IMG);
 }
 
 
-const putCharacters = async () => {
+const putCharacters = async (api) => {
+  await removeCurrentElements();
   await loading();
-  let characters = await fetchCharacters(ALL_API);
+  const targetAPI = APIs.find((API) => API.category == api)
+  let characters = await fetchCharacters(targetAPI.url);
   await createElements(characters);
 }
 
-putCharacters();
+const putCharacterList = () => {
+  $INPUTS.forEach($input => {
+    $input.addEventListener("click", () => {
+      if (!$input.checked) return;
+      putCharacters($input.id);
+    })
+  })
+}
 
-// loading();
-// fetchCharacters(ALL_API);
-
-// const removePreviousLis =  () => {
-//   const $prevLis = document.querySelectorAll(".character");
-//   $prevLis.forEach($prevLi => {
-//     $UL_ALL.removeChild($prevLi);
-//   })
-// }
-
-// $INPUTS.forEach($input => {
-//   $input.addEventListener("click", () => {
-//     if (!$input.checked) return;
-//     removePreviousLis();
-//   })
-// });
-
-// window.addEventListener("load", { handleEvent: fecthApi(ALL_API) });
-// window.addEventListener("load", () => {
-//   console.log("load: リソースファイルを全て読み込みました。");
-//   loading();
-//   fetchCharacters(HASHIRA_API);
-// });
+putCharacterList();
